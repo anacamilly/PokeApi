049079cb17ee15a0998ee5a86861d79173cb64eb
Ana Camilly <camillyguedes2@gmail.com>
2022-12-08 17:17:29 -0300

    repository

---
+++ app/src/main/java/ufrn/anacamilly/pokeapi/repository/PokeRepository.kt
@@ -0,0 +1,10 @@
+package ufrn.anacamilly.pokeapi.repository
+
+import ufrn.anacamilly.pokeapi.data.model.PokeItemListModel
+import ufrn.anacamilly.pokeapi.data.model.PokeModel
+
+interface PokeRepository {
+
+    suspend fun getPokeList(): PokeItemListModel
+    suspend fun getPokemon(url:String): PokeModel
+}
